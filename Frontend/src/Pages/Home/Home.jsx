60db3ca3f402e220f6032a89ac559965ad8ebcb3
smitthakar <smitthakar199@gmail.com>
2025-02-18 17:13:22 +0530

    Home.jsx Modified

---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import Navbar from "../src/Components/Navbar/Navbar";
+import HeroSlider from "./Components/Header/Header";
+import Services from "./Components/Services/Services";
+import GlobalPresence from "./Components/GlobalPresence/GlobalPresence ";
+import IndustriesCarousel from "./Components/IndustriesServed/IndustriesServed";
+import AboutUsSection from "./Components/AboutUsSection/AboutUsSection";
+import WhyChooseUsSection from "./Components/WhyChooseUsSection/WhyChooseUsSection";
+import BlogSection from "./Components/BlogSection/BlogSection";
+import NewProduct from "./Components/NewProduct/NewProduct";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import CallToAction from "./Components/NeedHelp/NeedHelp";
+import Footer from "./Components/Footer/Footer";
+
+export const Home = () => {
+  return (
+    <>
+    <Navbar />
+      <HeroSlider />
+      <Services />
+      <GlobalPresence />
+      <IndustriesCarousel />
+      <AboutUsSection />
+      <WhyChooseUsSection />
+      <NewProduct />
+      <Testimonials />
+      <BlogSection />
+      <CallToAction />
+      <Footer />
+    </>
+  )
+}
