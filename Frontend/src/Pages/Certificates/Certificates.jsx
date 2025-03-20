d1fa3bb44459c7e98b91c2fe42f32ee59dbfe30b
smitthakar <smitthakar199@gmail.com>
2025-03-20 17:04:15 +0530

    certificate image change

--- Frontend/src/Pages/Certificates/Certificates.jsx
+++ Frontend/src/Pages/Certificates/Certificates.jsx
@@ -1,29 +1,43 @@
-import certificateImage from "../../assets/certificate.png"; 
+import  { useState } from "react";
+import certificateImage from "../../assets/certificate.svg"; 
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import "./Certificates.css";
 
 const Certificates = () => {
+  const [activeImage, setActiveImage] = useState(null);
+
   return (
     <>
-    <Navbar/>
-    <div className="certificate-wrapper">
-  <h2 className="certificate-title">Certificates</h2>
+      <Navbar />
+      <div className="certificate-wrapper">
+        <h2 className="certificate-title">Certificates</h2>
 
-  <div className="certificate-grid">
-    {[...Array(7)].map((_, index) => (
-      <div key={index} className="certificate-card">
-        <img
-          src={certificateImage}
-          alt="Certificate"
-          className="certificate-img"
-        />
+        <div className="certificate-grid">
+          {[...Array(7)].map((_, index) => (
+            <div
+              key={index}
+              className="certificate-card"
+              onClick={() => setActiveImage(certificateImage)}
+            >
+              <img
+                src={certificateImage}
+                alt="Certificate"
+                className="certificate-img"
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-</div>
 
-    <Footer/>
+      {/* Zoom Modal */}
+      {activeImage && (
+        <div className="zoom-overlay" onClick={() => setActiveImage(null)}>
+          <img src={activeImage} alt="Zoomed Certificate" className="zoom-image" />
+        </div>
+      )}
+
+      <Footer />
     </>
   );
 };
