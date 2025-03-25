aeae0be56fd999597ab2dec435b8c419f40df752
= <smitthakar199@gmail.com>
2025-03-25 18:33:28 +0530

    qualityAssurance page css

--- Frontend/src/Pages/QualityAssurance/QualityAssurance.jsx
+++ Frontend/src/Pages/QualityAssurance/QualityAssurance.jsx
@@ -29,13 +29,13 @@ const QualityAssurance = () => {
             <h1 className="quality-title">
               Quality Within The <br /> Organization
             </h1>
-            <p className="quality-paragraph">
+            <p className="quality-paragraph-one">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
-            <p className="quality-paragraph">
+            <p className="quality-paragraph-one">
               It has survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. It was
               popularised in the 1960s with the release of Letraset sheets
